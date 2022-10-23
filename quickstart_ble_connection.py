3701b6a824ecd369499ded746a0d934f7728a190
Andrew <protobioengineering@gmail.com>
2022-10-23 16:56:49 -0700

    Quickstart BLE connection script

---
+++ quickstart_ble_connection.py
@@ -0,0 +1,24 @@
+'''
+Minimal Bluetooth LE connection code.
+
+Requires Python3 and the Bleak library.
+'''
+
+
+import asyncio
+from bleak import BleakClient, BleakScanner
+
+
+# Can be UUID or MAC address
+# Add your device's address here.
+address = 'ABCDEFGG-1111-2222-3333-444455556666'
+
+async def main():
+    device = await BleakScanner.find_device_by_address(address)
+
+    async with BleakClient(device) as client:
+        print(f'Client connection = {client.is_connected}') # prints True or False
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
